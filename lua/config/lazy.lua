c99758829c1eb0823245f7e3c7fd928a7a52f69f
IlReSenzaNome <alexisvivas3@gmail.com>
2023-09-20 19:17:02 -0500

    add lazy-lock.jason and plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- Add LazyVim and import its plugins
-	{ "LazyVim/LazyVim", import = "lazyvim.plugins"},
-  }
+    { import = "plugins" },
+  }, 
 }) 
