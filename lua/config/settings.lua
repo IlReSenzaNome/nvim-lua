ac73eea136641ce18db35e94b5afa4c3f1df5b53
IlReSenzaNome <alexisvivas3@gmail.com>
2023-09-21 15:27:56 -0500

    add noice and lualine

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,19 +1,58 @@
 --------------------------------------
 -- NeoVim API Aliases
 --------------------------------------
-local cmd = vim.cmd -- Comands
-local exec = vim.api.nvim_exec --
-local fn = vim.fn --
-local g = vim.g -- Global values
-local opt = vim.opt -- General options
+local cmd = vim.cmd            -- Execute Vim Comands
+local exec = vim.api.nvim_exec -- Execute Vim Script
+local fn = vim.fn              -- Call Vim Functions
+local g = vim.g                -- Global values
+local opt = vim.opt            -- General options
+local au = vim.api.nvim_create_autocmd -- create autocomand
+local ag = vim.api.nvim_create_augroup -- Create autogroup
 
 --------------------------------------
 -- NeoVim General config
 --------------------------------------
-g.mapleader = ","
+g.mapleader = " "              -- leader to space
+opt.clipboard = 'unnamedplus'  -- Copy/Paste to sytem clipboard 
+opt.swapfile = false           -- Don't use swapfile
 
 --------------------------------------
 -- NeoVim Interfaces
 --------------------------------------
 opt.number = true
 opt.relativenumber = true
+
+--------------------------------------
+-- Tabs, Ident
+--------------------------------------
+opt.expandtab = true           -- Use spaces Instead of tabs
+opt.shiftwidth = 4             -- Shift 4 spaces when tab
+opt.tabstop = 4                -- 1 Tab == 4 Spaces
+opt.smartindent = true         -- autoindent now lines 
+
+--------------------------------------
+-- Spell
+--------------------------------------
+local markdown_spell = ag("markdownSpell",{})
+au(
+	"fileType",
+	{
+		pattern = "markdown",
+		callback = function()
+			vim.opt.spelllang = "es"
+			vim.opt.spell = true
+		end,
+		group = markdown_spell
+	}
+)
+au(
+	{"BufRead", "BufNewFile"},
+	{
+		pattern = "*.md",
+		callback = function()
+			vim.opt.spelllang = "es"
+			vim.opt.spell = true
+		end,
+		group = markdown_spell
+	}
+)
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,37 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup {
+            options = {
+                theme = "auto",
+                icons_enabled = true,
+                globalstatus = true,
+            },
+            sections = {
+                lualine_a = {{"mode", upper = true}},
+                lualine_b = {{"branch", icon = ""}, "db_ui#statusline"},
+                lualine_c = {{"filename", file_status = true, path = 1}},
+                lualine_x = {},
+                lualine_y = {"filetype"},
+                lualine_z = {"location"},
+            },
+            winbar = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {"filename"},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+            inactive_winbar = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {"filename"},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {}, 
+            },
+        }
+    end,
+}
